a5e965e7ff46855a725b11f0c8a8216261d9f146
sholeyegbengs <35511200+sholeyegbengs@users.noreply.github.com>
2020-08-09 15:16:59 +0100

    Add files via upload

---
+++ covid19/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import fetch from './components/fetch'
+import './App.css';
+import Navbar from './components/Navbar';
+import WorldTable from './components/WorldTable';
+import Cards from './components/Cards';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Cards/>
+     <WorldTable/>
+    </div>
+  );
+}
+
+export default App;
